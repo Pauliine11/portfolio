a619ab2c43c4f3d05c540aebb626733c7d92f2df
Pauliine11 <paulinevires@.com>
2025-08-20 16:45:24 +0200

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,90 @@
+// Dark - Light
 
-//Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach
-(anchor => {
-    anchor.addEventListener("click", function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute
-        ('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+let darkModeEnabled = localStorage.getItem('darkModeEnabled') === 'true';
+
+const enableDarkMode = () => {
+    darkModeEnabled = true;
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'true');    
+}
+
+const disableDarkMode = () => {
+    darkModeEnabled = false;
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkModeEnabled', 'fasle');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    darkModeButton.checked = darkModeEnabled;
+    document.body.classList.toggle('dark-mode', darkModeEnabled);
 });
 
-// Navbar scroll effect
-window.addEventListener('scroll', () => {
-    const navbar = document.querySelector('.navbar');
-    wincdow.scrollY > 50 ?
-        navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.98)' :
-        navbar.style.backgroundColor = 'rgba(10, 10, 10, 0.95)';
+const darkModeButton = document.getElementById('darkModeButton');
+
+darkModeButton.addEventListener('change', () => {
+    if(darkModeButton.checked) {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
+
+// Menu
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('nav ul');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navList.classList.toggle('active');
+});
+
+// Multiple text
+
+const typed = new Typed('.multiple', {
+    strings: ["Développeuse web junior", 'Front-End', 'Back-End'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backlDelay: 1000,
+    loop: true
+});
+
+// Header shadow on scroll effect
+
+const header = document.querySelector('header');
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 0) {
+        header.style.boxShadow = '0 2px 4px rgba(0, 0, 0, 0.1)';
+    } else {
+        header.style.boxShadow = 'none'; 
+    }
+});
+
+// Scroll section active link
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll= () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            document.querySelector('header nav ul li a[href="#' + id + '"]').classList.add('active');
+        }
+    });
+};
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburger.classList.remove('active');
+        navList.classList.remove('active');
+    });
 });
